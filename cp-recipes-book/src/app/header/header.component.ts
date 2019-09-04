de98946afb336ca35b05ddafa5621b84f4d871ed
Serhii Plyska <blysca@newtonideas.com>
2019-09-04 18:45:11 +0300

    completed "Section 6: 85. Passing Recipe Data with Property Binding"

--- cp-recipes-book/src/app/header/header.component.ts
+++ cp-recipes-book/src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   collapsed = true;
+  @Output() featureSelected = new EventEmitter<string>();
+  activeItem: string;
   
-  constructor() { }
-
-  ngOnInit() {
+  
+  constructor() {
   }
-
+  
+  ngOnInit() {
+    this.activeItem = 'recipes';
+    this.featureSelected.emit(this.activeItem);
+  }
+  
+  onSelect(feature: string) {
+    this.activeItem = feature;
+    this.featureSelected.emit(this.activeItem);
+  }
 }
