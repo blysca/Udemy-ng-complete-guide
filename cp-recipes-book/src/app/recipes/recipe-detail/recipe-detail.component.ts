ed06e561a8a5bc370263eda47ea6ea42f6bb4013
Serhii Plyska <blysca@newtonideas.com>
2019-09-05 11:13:45 +0300

    completed "Section 6: 86. Passing Data with Event and Property Binding (Combined)"

--- cp-recipes-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ cp-recipes-book/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.scss']
 })
-export class RecipeDetailComponent implements OnInit {
-
+export class RecipeDetailComponent implements OnInit, OnChanges {
+  manageRecipe: boolean = false;
+  
+  @Input() recipe: Recipe;
+  
   constructor() { }
 
   ngOnInit() {
   }
-
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('*** recipe = ', this.recipe);
+  }
 }
--- cp-recipes-book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cp-recipes-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import {Recipe} from '../recipe.model';
 
@@ -8,12 +8,23 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() showRecipe = new EventEmitter<Recipe>();
+  
   recipes: Recipe[] = [
-    new Recipe('A test Recipe', 'this is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
-    new Recipe('A test Recipe', 'this is a test', 'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('Beef Wellington', 'Beef Wellington is a preparation of fillet steak coated with pâté (often pâté de foie gras) and duxelles, which is then wrapped in parma ham, puff pastry and baked. Some recipes include wrapping the coated meat in a crêpe to retain the moisture and prevent it from making the pastry soggy.', 'https://upload.wikimedia.org/wikipedia/commons/a/ac/Beef_Wellington_-_Crosscut.jpg'),
+    new Recipe('Alambre', 'Alambre (About this sounda\'lambre) is a popular Mexican dish consisting of grilled beef topped with chopped bacon, bell peppers, onions, cheese, salsa and, in some variations, avocado. It is usually served with freshly made corn or flour tortillas. The most common ingredient is beef, and other kinds of meat such as chicken or pork are also used. Some recipes even substitute chopped ham or chorizo instead of the bacon. Alambres are popular in many parts of Mexico, especially in Mexico City, Oaxaca, and among Mexican-American populations across the United States.', 'https://upload.wikimedia.org/wikipedia/commons/1/1d/Alambre.JPG'),
+    new Recipe('Pavlova (cake)', 'Pavlova is a meringue-based dessert named after the Russian ballerina Anna Pavlova.[1] It is a meringue dessert with a crisp crust and soft, light inside, usually topped with fruit and whipped cream.[2] The name is pronounced /pævˈloʊvə/, or like the name of the dancer, which was /ˈpɑːvləvə/.', 'https://upload.wikimedia.org/wikipedia/commons/b/bd/Christmas_pavlova.jpg'),
   ];
+  
+  activeIndex:number;
+  
+  
   constructor() {}
 
   ngOnInit() {}
-
+  
+  activateItem(ev: boolean, i: number) {
+    this.activeIndex = i;
+    this.showRecipe.emit(this.recipes[i]);
+  }
 }
