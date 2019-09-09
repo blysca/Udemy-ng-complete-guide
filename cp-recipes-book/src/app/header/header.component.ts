5da2121798a0415baec8667346db2140adf47411
Serhii Plyska <blysca@newtonideas.com>
2019-09-09 14:28:09 +0300

    completed "Section 12: Course Project - Routing"

--- cp-recipes-book/src/app/header/header.component.ts
+++ cp-recipes-book/src/app/header/header.component.ts
@@ -5,23 +5,6 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   collapsed = true;
-  @Output() featureSelected = new EventEmitter<string>();
-  activeItem: string;
-  
-  
-  constructor() {
-  }
-  
-  ngOnInit() {
-    this.activeItem = 'recipes';
-    // this.activeItem = 'shopping-list';
-    this.featureSelected.emit(this.activeItem);
-  }
-  
-  onSelect(feature: string) {
-    this.activeItem = feature;
-    this.featureSelected.emit(this.activeItem);
-  }
 }
