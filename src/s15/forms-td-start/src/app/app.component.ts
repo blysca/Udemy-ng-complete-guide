0c8b28d816db97f6684118a987f645b43d0befdb
Serhii Plyska <blysca@newtonideas.com>
2019-09-11 13:56:24 +0300

    completed "Section 15: 198. TD: Resetting Forms"

---
+++ src/s15/forms-td-start/src/app/app.component.ts
@@ -0,0 +1,61 @@
+import {Component, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  @ViewChild('formElement', {static: true}) signupForm;
+  defaultQuestion = 'pet';
+  answer = '';
+  genders=['male','female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+  
+  submitted = false;
+  
+  suggestUserName() {
+    const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     usernamee: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+  }
+  
+  /*onSubmit(form: NgForm) {
+    console.log('*** submitted! ');
+    console.log('*** form= ', form);
+  }*/
+  
+  onSubmit() {
+    console.log('*** @viewChild ');
+    console.log('*** form= ', this.signupForm);
+  
+    this.submitted = true;
+    
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    
+    this.signupForm.reset();
+  }
+}
